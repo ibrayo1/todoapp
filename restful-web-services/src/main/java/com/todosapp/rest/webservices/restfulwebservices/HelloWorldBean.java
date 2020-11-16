b094c7a08a2be4d0501f4388835d4f466f2df914
ibrayo1 <ibrayo1128@gmail.com>
2020-11-16 14:00:09 -0600

    added spring boot restful webservice

---
+++ restful-web-services/src/main/java/com/todosapp/rest/webservices/restfulwebservices/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.todosapp.rest.webservices.restfulwebservices;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    // setter method: sets message
+    public void setMessage(String msg){
+        this.message = msg;
+    }
+
+    // getter method: returns message
+    public String getMessage(){
+        return this.message;
+    }
+
+    @Override
+    public String toString(){
+        return String.format("HelloWorldBean [message=%s]", message);
+    }
+}
