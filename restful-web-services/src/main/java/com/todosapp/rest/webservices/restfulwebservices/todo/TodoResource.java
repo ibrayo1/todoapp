f61352404d51d55e71a514693094fb83fc6978a4
ibrayo1 <ibrayo1128@gmail.com>
2020-11-22 22:32:59 -0600

    implemented todo delete

--- restful-web-services/src/main/java/com/todosapp/rest/webservices/restfulwebservices/todo/TodoResource.java
+++ restful-web-services/src/main/java/com/todosapp/rest/webservices/restfulwebservices/todo/TodoResource.java
@@ -1,10 +1,8 @@
 package com.todosapp.rest.webservices.restfulwebservices.todo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,5 +16,18 @@ public class TodoResource {
     @GetMapping("/users/{username}/todos")
     public List<Todo> getAllTodos(@PathVariable String username){
         return todoService.findAll();
+    }
+
+    // DELETE /users/{username}/todos/{id}
+    @DeleteMapping("/users/{username}/todos/{id}")
+    public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id){
+
+        Todo todo = todoService.deleteById(id);
+
+        if (todo != null){
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
