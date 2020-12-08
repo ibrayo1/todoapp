e5a0128e60bcdce2e77f21f15416aa0c071026e0
ibrayo1 <ibrayo1128@gmail.com>
2020-12-07 22:26:12 -0600

    added jwt for use in frontend

--- todo/src/app/login/login.component.ts
+++ todo/src/app/login/login.component.ts
@@ -52,4 +52,19 @@ export class LoginComponent implements OnInit {
     );
   }
 
+  handleJwtAuthLogin(): any {
+    this.basicAuthenticationService.executeJwtAuthenticationService(this.username, this.password)
+    .subscribe(
+      data => {
+        console.log(data);
+        this.router.navigate(['welcome', this.username]);
+        this.invalidLogin = false;
+      },
+      error => {
+        console.log(error);
+        this.invalidLogin = true;
+      }
+    );
+  }
+
 }
--- todo/src/app/service/basic-authentication.service.ts
+++ todo/src/app/service/basic-authentication.service.ts
@@ -13,6 +13,23 @@ export class BasicAuthenticationService {
 
   constructor(private http: HttpClient) { }
 
+  executeJwtAuthenticationService(username: string, password: string): any {
+
+    return this.http.post<any>(
+      `${API_URL}/authenticate`, {
+        username,
+        password
+      }).pipe(
+        map(
+          data => {
+            sessionStorage.setItem(AUTHENTICATED_USER, username);
+            sessionStorage.setItem(TOKEN, `Bearer ${data.token}`);
+            return data;
+          }
+        )
+      );
+  }
+
   executeAuthenticationService(username: string, password: string): any {
     const basicAuthHeaderString = 'Basic ' + window.btoa(username + ':' + password);
 
